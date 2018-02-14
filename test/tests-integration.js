2d9f196c045b1a6138d49373a23ac6fe97f966ea
deliacodes <deliacodes@gmail.com>
2018-02-13 22:17:43 -0800

    Roughed out inserting test data.

--- test/tests-integration.js
+++ test/tests-integration.js
@@ -19,3 +19,21 @@ const {
 const {
   TEST_DATABASE_URL
 } = require('../config');
+
+const seedBlogData = () => {
+  const seedData = [];
+
+  for (let i = 1; i <= 10; i++) {
+    seedData.push(generateBlogPostData());
+  }
+  return BlogPost.insertMany(seedData);
+};
+
+const generateBlogPostData = () => {
+  return {
+    //come back to this
+    title: faker.title,
+    content: faker.lorem,
+    author: faker.author
+  };
+}
