48464c89c923592b5bced08ff00f1efbc075ef38
deliacodes <deliacodes@gmail.com>
2018-02-13 22:07:52 -0800

    Set up file for creating fake data.

--- test/tests-integration.js
+++ test/tests-integration.js
@@ -7,3 +7,15 @@ const faker = require('faker');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
+
+const {
+  BlogPost
+} = require('../models');
+const {
+  app,
+  runServer,
+  closeServer
+} = require('../server');
+const {
+  TEST_DATABASE_URL
+} = require('../config');
